97182eb2e7b19f2041ddcedd0776a41468560d74
Jeffrey Martinez <martinezjf2@gmail.com>
2020-02-04 00:47:28 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -1,5 +1,16 @@
 # require libraries/modules here
+require "nokogiri"
+# require "open-uri"
+require "pry"
+
 
 def create_project_hash
+  html = File.read('fixtures/kickstarter.html')
+  kickstarter = Nokogiri::HTML(html)
+  binding.pry
+  kickstarter.css("li.project.grid_4").first
   # write your code here
+
 end
+
+create_project_hash
